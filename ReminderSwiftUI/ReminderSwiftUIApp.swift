58ca99a103dd406db74b7ddcb9b3ad75fa5c2ab2
HardiB-Salih <hardib.salih@gmail.com>
2024-05-31 16:12:25 +0300

    first Commit

---
+++ ReminderSwiftUI/ReminderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderSwiftUIApp.swift
+//  ReminderSwiftUI
+//
+//  Created by HardiB.Salih on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct ReminderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
